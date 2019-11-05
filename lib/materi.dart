aaf763646fb8a3313755edc3fbc4275459700ff0
yogahermawan <yogahermawan43@gmail.com>
2019-11-05 23:25:59 +0700

    add card

--- lib/materi.dart
+++ lib/materi.dart
@@ -1,14 +1,27 @@
-
 import 'package:flutter/material.dart';
 
 class materi extends StatelessWidget {
-  static final routeName= "/materi";
+  static final routeName = "/materi";
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text("Center"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Materi Pembelajaran"),
+        backgroundColor: Colors.cyanAccent,
+      ),
+      body: ListView(
+        children: [1, 2, 3, 4]
+            .map((val) => Container(
+                  padding: EdgeInsets.all(8.0),
+                  margin: EdgeInsets.all(16),
+                  child: Card(
+                    elevation: 5.0,
+                    child: Text("Text"),
+                  ),
+                ))
+            .toList(),
+      ),
     );
   }
-  
 }
