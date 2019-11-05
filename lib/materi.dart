653c21c574ac4e307ba22e8e1b7e04f87eaf16ee
yogahermawan <yogahermawan43@gmail.com>
2019-11-05 22:42:38 +0700

    add widget materi

---
+++ lib/materi.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class materi extends StatelessWidget {
+  static final routeName= "/materi";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text("Center"),
+    );
+  }
+  
+}
