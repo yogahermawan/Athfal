75d2f8bfa0a4bcb3c4a1e0bfd2bd94059588e8cc
yogahermawan <yogahermawan43@gmail.com>
2019-11-05 23:52:21 +0700

    rename class

--- lib/Materi.dart
+++ lib/Materi.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class materi extends StatelessWidget {
+class MateriPage extends StatelessWidget {
   static final routeName = "/materi";
   @override
   Widget build(BuildContext context) {
@@ -11,15 +11,14 @@ class materi extends StatelessWidget {
         backgroundColor: Colors.cyanAccent,
       ),
       body: ListView(
-        children: [1, 2, 3, 4]
-            .map((val) => Container(
-                  padding: EdgeInsets.all(8.0),
-                  margin: EdgeInsets.all(16),
-                  child: Card(
-                    elevation: 5.0,
-                    child: Text("Text"),
-                  ),
-                ))
+        children: new List<int>.generate(3, (i) => i + 1)
+            .map((val) => Card(
+              elevation: 5.0,
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Text"),
+              ),
+            ))
             .toList(),
       ),
     );
